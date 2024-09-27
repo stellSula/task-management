8dee13aa154e7e0cbd03de9385caabf66fddc737
Sultan <zhusupbekovst@gmail.com>
2024-09-27 08:04:56 +0600

    Logging for requests and responses has been added

--- src/main/java/com/example/task_management/controllers/TaskController.java
+++ src/main/java/com/example/task_management/controllers/TaskController.java
@@ -5,6 +5,8 @@ import com.example.task_management.dtos.TaskDto;
 import com.example.task_management.enums.Status;
 import com.example.task_management.exeptions.EmptyTaskListException;
 import com.example.task_management.services.TaskService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,6 +18,7 @@ import java.util.List;
 public class TaskController {
 
     private final TaskService taskService;
+    private static final Logger logger = LoggerFactory.getLogger(TaskController.class);
 
     public TaskController(TaskService taskService) {
         this.taskService = taskService;
@@ -23,17 +26,23 @@ public class TaskController {
 
     @PostMapping("/create")
     public ResponseEntity<Long> createTask(@RequestBody TaskDto taskDto) {
-        return ResponseEntity.ok(taskService.createTask(taskDto));
+        logger.info("Creating task with info: {}", taskDto.title());
+        Long taskId = taskService.createTask(taskDto);
+        logger.info("Task created with id: {}", taskId);
+        return ResponseEntity.ok(taskId);
     }
 
     @GetMapping("/get-all")
     public ResponseEntity<List<RequestTaskDto>> getAllTasks() {
+        logger.info("Fetching all tasks");
         List<RequestTaskDto> tasks = taskService.getAllTasks();
 
         if (tasks.isEmpty() || tasks == null) {
+            logger.warn("No tasks found");
             throw new EmptyTaskListException();
         }
 
+        logger.info("Retrieved {} tasks", tasks.size());
         return ResponseEntity.ok(tasks);
     }
 
@@ -45,16 +54,24 @@ public class TaskController {
             @RequestParam(required = false) Status status,
             @RequestParam(required = false) Instant deadline,
             @RequestParam(name = "executor_id", required = false) Long executorId) {
-        return ResponseEntity.ok(taskService.updateTask(id, title, description, status, deadline, executorId));
+        logger.info("Updating task with id: {}", id);
+        Long updatedTaskId = taskService.updateTask(id, title, description, status, deadline, executorId);
+        logger.info("Task with id:{} was updated", updatedTaskId);
+        return ResponseEntity.ok(updatedTaskId);
     }
 
     @DeleteMapping
     public void deleteTask(@RequestParam Long id) {
+        logger.info("Deleting task with id: {}", id);
         taskService.deleteTask(id);
+        logger.info("Task with id: {} was deleted", id);
     }
 
     @GetMapping("/get-by-id")
     public ResponseEntity<RequestTaskDto> getTaskById(@RequestParam Long id) {
-        return ResponseEntity.ok(taskService.getTaskById(id));
+        logger.info("Fetching task with id: {}", id);
+        RequestTaskDto requestTaskDto = taskService.getTaskById(id);
+        logger.info("Retrieved task: {}", requestTaskDto.title());
+        return ResponseEntity.ok(requestTaskDto);
     }
 }
--- src/main/java/com/example/task_management/controllers/UserController.java
+++ src/main/java/com/example/task_management/controllers/UserController.java
@@ -2,6 +2,8 @@ package com.example.task_management.controllers;
 
 import com.example.task_management.dtos.UserDto;
 import com.example.task_management.services.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     public UserController(UserService userService) {
         this.userService = userService;
@@ -20,7 +23,10 @@ public class UserController {
 
     @PostMapping("/create")
     public ResponseEntity<Long> createUser(@RequestBody UserDto userDto) {
-        return ResponseEntity.ok(userService.createUser(userDto));
+        logger.info("Creating user with info: {}", userDto.userName());
+        Long userId = userService.createUser(userDto);
+        logger.info("Task created with id: {}", userId);
+        return ResponseEntity.ok(userId);
     }
 
 }
